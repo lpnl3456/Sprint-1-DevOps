d011aa2dd3331ff561700b78369ef4d433091b83
lpnl3456 <luke.peddle@keyin.com>
2024-10-21 20:06:17 -0230

    Fixed issue

---
+++ src/main/java/MainProgram.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+public class MainProgram {
+    public static void main(String[] args) {
+        SpringApplication.run(MainProgram.class, args);
+    }
+}
