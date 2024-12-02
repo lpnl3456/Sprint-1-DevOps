719290fa7551acc53c6569bd40b01418f8ead79c
Samantha Thorne <150044477+13Thornes@users.noreply.github.com>
2024-12-02 16:28:29 -0330

    fixed takeoff files

--- src/main/java/com/keyin/rest/TakeOff/TakeOffRepository.java
+++ src/main/java/com/keyin/rest/TakeOff/TakeOffRepository.java
@@ -1,11 +1,14 @@
 package com.keyin.rest.TakeOff;
 
 
+import com.keyin.rest.Airport.AirPort;
 import com.keyin.rest.Landing.Landing;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TakeOffRepository extends CrudRepository<TakeOff, Long> {
-
+    public List<TakeOff> findByTakeOffLocation(AirPort airport);
 }
